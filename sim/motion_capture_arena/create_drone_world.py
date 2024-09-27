bfd11ecdf0bcc92ad2cd3eeec78f7e801e8e2072
Stijn W <swaltmann98@gmail.com>
2024-09-27 12:01:08 +0200

    Add outside of drone cage to gazebo
    This was way too hard, and the end result is a little ugly, but it works:) probably won't be changed much anyway

--- sim/motion_capture_arena/create_drone_world.py
+++ sim/motion_capture_arena/create_drone_world.py
@@ -4,7 +4,121 @@
 # care too much for that, yo ushould not have to interact directly with
 # it (although you can if you want of course)
 
+import numpy as np
 import xml.etree.ElementTree as ET
+
+def calculate_pose_and_size_3d(p1, p2, p3, p4):
+    """ Calculate pose and size of rectangle from its corners """
+    # Convert points to numpy arrays for easier vector calculations
+    p1 = np.array(p1)
+    p2 = np.array(p2)
+    p3 = np.array(p3)
+    p4 = np.array(p4)
+
+    points = p1, p2, p3, p4
+
+    # The next 20 lines are just to select the right vertices of the rectangle.
+    # Someone smarter than I am can probably do it quicker ¯\_(ツ)_/¯
+    vectors = np.zeros((6, 3))
+    count = 0
+    for i in range(len(points)):
+        for j in range(i + 1, len(points)):
+            vectors[count] = points[i] - points[j]
+            count += 1
+
+    lengths = np.linalg.norm(vectors, axis=1)
+
+    side_length = np.sort(np.unique(lengths))
+
+    if len(side_length) == 2:  # perfect square
+        width, length = side_length[0], side_length[0]
+    elif len(side_length) == 3:  # perfect rectangle
+        length = side_length[0]
+        width = side_length[1]
+    else:  # not a rectangle
+        raise f"This is not a rectangle: {points}"
+    
+    # Calculate the center of the rectangle (average of all four points)
+    center = (p1 + p2 + p3 + p4) / 4
+    
+    print(f"Comparing {p1[2], p2[2], p3[2]}")
+    if np.isclose(p1[2], p2[2]) and np.isclose(p2[2], p3[2]):  # means it is not a wall
+        # Floor case: no roll or pitch, yaw is the angle between vec1 and the x-axis
+        yaw = 0 #np.arctan2(vec1[1], vec1[0])  # Orientation around z-axis
+        roll = 0
+        pitch = 0
+    else:
+        # Lazily hard coded these angles :) Only straight boxes for now
+        if np.isclose(p1[0], p2[0]) and np.isclose(p2[0], p3[0]):
+            yaw = 0 # np.arctan2(vec1[1], vec1[0])  # Calculate yaw based on width vector
+            roll = 0
+            pitch = np.pi / 2 
+        else:
+            # Wall case: pitch by pi/2 to make it stand up, adjust yaw for rotation
+            yaw = 0 # np.arctan2(vec1[1], vec1[0])  # Calculate yaw based on width vector
+            roll = np.pi / 2  # Wall stands straight, need roll due to orientation
+            pitch = 0  
+
+    # The final pose consists of the center coordinates and the Euler angles (roll, pitch, yaw)
+    pose = (center[0], center[1], center[2], roll, pitch, yaw)
+    size = (width, length)
+
+
+
+    return pose, size    
+
+
+def create_rectangle(pose, size, thickness, suffix):
+    """Create rectangle (useful for walls, ceiling)
+    
+    Taking into account that Gazebo uses the pose of an object to center 
+    it on that pose. Not taking the thickness or direction of that 
+    thickness into account
+    """
+    x, y, z, r, p, yaw = pose  # x, y, z, roll, pitch, yaw
+    width, length = size
+
+    # x, y, z = 0, 0, 0
+
+    pose_str = f'{x} {y} {z} {r} {p} {yaw}'
+
+    rect_name = f'rect_{suffix}'
+    rectangle = ET.SubElement(world, 'model', name=rect_name)
+
+    ET.SubElement(rectangle, 'static').text = 'true'
+
+    # pose = ET.SubElement(rectangle, 'pose')
+    # pose.text = pose_str 
+
+    link = ET.SubElement(rectangle, 'link', name='link')
+
+    # Add a collision element (for the floor's physical collisions)
+    collision = ET.SubElement(link, 'collision', name='collision')
+    collision_pose = ET.SubElement(collision, 'pose')
+    collision_pose.text = pose_str  # Collision's position same as the pose above
+
+    collision_geometry = ET.SubElement(collision, 'geometry')
+    collision_box = ET.SubElement(collision_geometry, 'box')
+
+    size = ET.SubElement(collision_box, 'size')
+    size.text = f'{length} {width} {thickness}'  # 0.01 m thick floor
+
+    visual = ET.SubElement(link, 'visual', name='visual')
+    visual_pose = ET.SubElement(visual, 'pose')
+    visual_pose.text = pose_str
+
+    visual_geometry = ET.SubElement(visual, 'geometry')
+    visual_box = ET.SubElement(visual_geometry, 'box')
+
+    visual_size = ET.SubElement(visual_box, 'size')
+    visual_size.text = size.text # Same size as the collision box
+
+    # Add material for the black color
+    visual_material = ET.SubElement(visual, 'material')
+    visual_ambient = ET.SubElement(visual_material, 'ambient')
+    visual_ambient.text = '0.0 0.0 0.0 1.0'  # Black color with full opacity
+    visual_diffuse = ET.SubElement(visual_material, 'diffuse')
+    visual_diffuse.text = '0.0 0.0 0.0 1.0'  # Black color with full opacity
 
 # Create the root element for the SDF
 sdf = ET.Element('sdf', version='1.7')
@@ -130,16 +244,14 @@ plugin = ET.SubElement(gui, "plugin",
                        filename="EntityTree", name="Entity tree")
 
 
-# Create the light element
+# Lighting
 light = ET.SubElement(world, 'light', type='directional', name='sun')
 
-# Set light properties
 ET.SubElement(light, 'cast_shadows').text = 'true'
 ET.SubElement(light, 'pose').text = '0 0 10 0 0 0'
 ET.SubElement(light, 'diffuse').text = '0.8 0.8 0.8 1'
 ET.SubElement(light, 'specular').text = '0.2 0.2 0.2 1'
 
-# Create the attenuation subelement
 attenuation = ET.SubElement(light, 'attenuation')
 ET.SubElement(attenuation, 'range').text = '1000'
 ET.SubElement(attenuation, 'constant').text = '0.9'
@@ -147,6 +259,106 @@ ET.SubElement(attenuation, 'linear').text = '0.01'
 ET.SubElement(attenuation, 'quadratic').text = '0.001'
 
 ET.SubElement(light, 'direction').text = '-0.5 0.1 -0.9'
+
+#################################
+#                               # 
+#   World building starts here  #
+#                               #
+#################################
+
+# Size of the motion capture arena:
+length = 16  # meter
+height = 8  # meter
+width = 8  # meter
+
+
+arena_corners = np.zeros((8,3))
+
+for i, corner in enumerate(arena_corners):
+    # Simple way to get the coordinates of a rectangle of specified size
+    # It cannot be rotated (by why would it)
+    if i % 2 != 0:
+        corner[0] = width / 2
+    else:
+        corner[0] = -width / 2
+
+    if i in [1, 2, 5, 6]:
+        corner[1] = length / 2
+    else:
+        corner[1] = -length / 2
+    
+    if i < 4 :
+        corner[2] = 0
+    else:
+        corner[2] = height
+
+# From this list, grab the 6 sides. Since it is constructed in a 
+# specific way, we can assume which point goes where
+arena_sides = [arena_corners[:4],
+               arena_corners[4:],
+               arena_corners[[0, 3, 4, 7]],
+               arena_corners[[1, 2, 5, 6]],
+               arena_corners[[1, 3, 5, 7]],
+               arena_corners[[0, 2, 4, 6]]]
+
+for i, side in enumerate(arena_sides):
+    pose, size = calculate_pose_and_size_3d(*side)
+
+    if i == 0:
+        suffix = 'floor'
+    elif i == 1:
+        suffix = "ceiling"
+    else:
+        suffix = f"wall{i-2}"
+
+    create_rectangle(pose, size, 0.01, suffix)
+
+# # Create the model for the floor
+
+# floor = ET.SubElement(world, 'model', name='floor')
+
+# ET.SubElement(floor, 'static').text = 'true'
+
+# pose = ET.SubElement(floor, 'pose')
+# pose.text = '0 0 0 0 0 0' 
+
+# link = ET.SubElement(floor, 'link', name='link')
+
+# # Add a collision element (for the floor's physical collisions)
+# collision = ET.SubElement(link, 'collision', name='collision')
+# collision_pose = ET.SubElement(collision, 'pose')
+# collision_pose.text = pose.text  # Collision's position same as the pose above
+
+# collision_geometry = ET.SubElement(collision, 'geometry')
+# collision_box = ET.SubElement(collision_geometry, 'box')
+
+# size = ET.SubElement(collision_box, 'size')
+# size.text = f'{length} {width} 0.01'  # 0.01 m thick floor
+
+# visual = ET.SubElement(link, 'visual', name='visual')
+# visual_pose = ET.SubElement(visual, 'pose')
+# visual_pose.text = pose.text 
+
+# visual_geometry = ET.SubElement(visual, 'geometry')
+# visual_box = ET.SubElement(visual_geometry, 'box')
+
+# visual_size = ET.SubElement(visual_box, 'size')
+# visual_size.text = size.text # Same size as the collision box
+
+# # Add material for the black color
+# visual_material = ET.SubElement(visual, 'material')
+# visual_ambient = ET.SubElement(visual_material, 'ambient')
+# visual_ambient.text = '0.0 0.0 0.0 1.0'  # Black color with full opacity
+# visual_diffuse = ET.SubElement(visual_material, 'diffuse')
+# visual_diffuse.text = '0.0 0.0 0.0 1.0'  # Black color with full opacity
+
+# Create walls:
+
+
+
+
+
+
 
 
 
